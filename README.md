63dcf07e675248ae097dec7d1c55658d6746ffd9
khaled301 <khaledhasan301@gmail.com>
2020-03-12 17:50:15 +0600

    first commit upto depreciated Symfony local server setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# CREDIT
+
+[ OverSeas Media ](https://www.youtube.com/channel/UCPsBHZLaXkZ_mmapD8vBLXg)
+
+[ freeCodeCamp ](https://www.youtube.com/watch?v=Bo0guUbL5uo)
+
+
+## IMPORTANT
+
+This is an exercise project.
+
+Note: .env file is not included.
+
---
+++ src/Controller/MainController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return new HttpFoundation\Response('<h1>Symfony Tutorial from freeCodeCamp!</h1>');
+    }
+
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     * @param HttpFoundation\Request $request
+     * @return HttpFoundation\Response
+     */
+    public function custom(HttpFoundation\Request $request) {
+
+//        dump($request->attributes->get('name'));
+//        dump($request->get('name'));
+//        dump($request);
+
+        $name = $request->get('name');
+        return new HttpFoundation\Response('<h1>Welcome to Custom Page ' . $name .' !</h1>');
+
+    }
+}
