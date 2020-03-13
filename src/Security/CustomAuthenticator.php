e74de96309acd46d77ace2ae96520afe20e1d055
khaled301 <khaledhasan301@gmail.com>
2020-03-14 01:04:32 +0600

    created RegistrationController with Registration View --- created form inside register() action using createFormBuilder() --- in password fied used RepeatedType & encrypt PassWord using UserPasswordEncoderInterface --- Redirect user from login page to post/index page using RedirectResponse in Security/CustomAuthenticator

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -95,7 +95,9 @@ class CustomAuthenticator extends AbstractFormLoginAuthenticator implements Pass
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+//        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+
+        return new RedirectResponse($this->urlGenerator->generate('post.index'));
     }
 
     protected function getLoginUrl()
